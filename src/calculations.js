bb4a0ec585860ae5b216ef364cd9776ed74f711c
Ozzy Cavendish <martin.chris.wood@gmail.com>
2021-12-31 20:07:41 +0000

    wip: implementing visuals using plotly

--- src/calculations.js
+++ src/calculations.js
@@ -1,19 +1,10 @@
-var Mortgage = {
-  deposit: 10000.0,                   // Initial deposit
-  years: 30,                          // Years over which mortgage is repaid
-  property_cost: 210000.0,
-  annual_interest_rate: 3.5,          // Interest rate is a %
-  annual_homeowner_insurance: 800,    // additional cost of homeowner's insurance
-}
-
 function fetchMortgageParameters() {
   // Retrieves the mortgage details from the form in the page
-  var mortgage = new FormData(document.getElementById("input_form"))
-  var deets = {}
+  var mortgage = new FormData(document.getElementById("input_form"));
+  var deets = {};
   for(var pair of mortgage.entries()) {
     deets[pair[0]] = pair[1];
-  }
-  console.log(deets)
+  };
   return deets;
 }
 
@@ -31,38 +22,53 @@ function calculateMonthlyPayments(mortgage) {
 
 function tabulatePayments(mortgage) {
   // Generate a monthly table of our what's paid to where
-  // Need to record month, payment, payment of interest, payment of principal, total
-  var df = {
-    month: [],
-    payment: [],
-    of_which_interest: [],
-    of_which_principal: [],
-    remaining_principal: [],
-    remaining_total: []
-  }
+  // Need to record month, payment, payment of interest, payment of principal, remaining to-pay and principal
   monthly_payment = calculateMonthlyPayments(mortgage);
   n_payments = mortgage['years'] * 12.0;
+  columns = ['month', 'payment', 'of_which_interest', 'of_which_principal', 'remaining_principal', 'remaining_total'];
+
+  var df = [];
   remaining_principal = mortgage['property_cost'] - mortgage['deposit'];
   remaining_total = n_payments * monthly_payment;
-
   for(let i = 0; i < n_payments; i++) {
+
     // Calculate the payment components
     interest_component = (mortgage['annual_interest_rate'] / 12.0 / 100.0) * remaining_principal;
     principal_component = monthly_payment - interest_component;
-
     remaining_principal = remaining_principal - principal_component;
     remaining_total = remaining_total - monthly_payment;
 
     // Record that month
-    df['month'].push(i+1);
-    df['payment'].push(monthly_payment.toFixed(2));
-    df['of_which_interest'].push(interest_component.toFixed(2));
-    df['of_which_principal'].push(principal_component.toFixed(2));
-    df['remaining_principal'].push(remaining_principal.toFixed(2));
-    df['remaining_total'].push(remaining_total.toFixed(2));
+    df.push({month: i+1,
+             payment: monthly_payment.toFixed(2),
+             of_which_interest: interest_component.toFixed(2),
+             of_which_principal: principal_component.toFixed(2),
+             remaining_principal: remaining_principal.toFixed(2),
+             remaining_total: remaining_total.toFixed(2)})
   }
+  // Add metadata, column names
+  df.columns = columns;
   return df;
 }
 
-df = tabulatePayments(Mortgage);
-document.getElementById("calc_space").innerHTML = prettifyTable(df);
+function plotPlan(df) {
+  trace_array = createTraces(df, xColumn="month");
+
+  // Add title/aesthetics/labels
+  var layout = {title: "Behold your hypothetical fate :D"};
+
+  // Behaviours
+  var config = {responsive: true};
+
+  Plotly.newPlot("plan_plot", trace_array, layout, config);
+}
+
+function main() {
+  // Primary, called from user button-click
+  mortgage = fetchMortgageParameters();
+  df = tabulatePayments(mortgage);
+  plotPlan(df);
+  // document.getElementById("calc_space").innerHTML = prettifyTable(df);
+}
+
+main()
--- src/helper.js
+++ src/helper.js
@@ -1,12 +1,42 @@
+function unpack(rows, key) {
+  // Utility, unpack an array of lists to a single array
+  return rows.map(function(row) {return row[key]; });
+}
+
 function prettifyTable(df) {
   // Convenience function essentially for printing out my tables to QA
-  keys = Object.keys(df);
-  nrows = df[keys[0]].length;
-  strout = "<br />" + keys.join(", ") + "<br />";
-  for(let i = 0; i < nrows; i++) {
-    values = [];
-    keys.forEach(key => values.push(df[key][i].toString()));
-    strout = strout + values.join(", ") + "<br />";
-  }
+  strout = "<br />" + df.columns.join(", ") + "<br />";   // Column names
+  df.map(function(row) {strout = strout + Object.values(row).join(", ") + "<br />"});  // Row values
   return strout;
+}
+
+function createTraces(data, xColumn="date", traceType="scatter") {
+  // Utility, unpacks a data structure into an array of trace objects for
+  // use with plotly
+  var columnNames = data['columns'];
+  var traces = [];
+
+  for(let i = 0; i < columnNames.length; i++) {
+    var name = columnNames[i];
+    if(name == xColumn) {continue; }
+    trace = {
+      name: name,
+      type: traceType,
+      x: unpack(data, xColumn),
+      y: unpack(data, name)
+    }
+    if (name.includes("smoothed") == false) {
+      trace['visible'] = "legendonly";
+    }
+    traces.push(trace);
+  }
+  return traces;
+}
+
+var Mortgage = {
+  deposit: 10000.0,                   // Initial deposit
+  years: 30,                          // Years over which mortgage is repaid
+  property_cost: 210000.0,
+  annual_interest_rate: 3.5,          // Interest rate is a %
+  annual_homeowner_insurance: 800,    // additional cost of homeowner's insurance
 }
